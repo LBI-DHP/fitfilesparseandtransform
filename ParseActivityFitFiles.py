1c9733fa7c28603f24ed955653977f5b00014ea4
Eva-Maria Strumegger <eva-maria.strumegger@fh-salzburg.ac.at>
2022-02-09 13:45:16 +0100

    add units to activity file

--- ParseActivityFitFiles.py
+++ ParseActivityFitFiles.py
@@ -1,21 +1,25 @@
-import fitparse # parse ANT/Garmin .FIT files
-import pandas # data analysis and manipulation 
-import sys # System-specific parameters and functions
-import os # using operating system dependent functionality
-from os import walk # generating directory tree
+import fitparse  # parse ANT/Garmin .FIT files
+import pandas  # data analysis and manipulation
+import sys  # System-specific parameters and functions
+import os  # using operating system dependent functionality
+from os import walk  # generating directory tree
 import datetime as dt
 
-def getSwVersion(fitfile): # defining a function
-    for element in fitfile.get_messages(): # get list of DataMessage record objects that are contained in this FitFile
+
+def getSwVersion(fitfile):  # defining a function
+    # get list of DataMessage record objects that are contained in this FitFile
+    for element in fitfile.get_messages():
         for d in element:
             if d.name == "software_version":
                 return d.value
 
-def getFileType(fitfile): 
-    for element in fitfile.get_messages(): 
+
+def getFileType(fitfile):
+    for element in fitfile.get_messages():
         for d in element:
             if d.name == "type":
                 return d.value
+
 
 def getTimeOffset(fitfile):
     for message in fitfile.get_messages("device_settings"):
@@ -24,28 +28,35 @@ def getTimeOffset(fitfile):
                 return data.value
     return 0
 
+
 def appendToDataframe(data_frame_name, fitfile, time_offset, dataframe):
     for message in fitfile.get_messages(data_frame_name):
         newRow = {}
         for data in message:
             if(data.name == "timestamp"):
                 utc_datetime = data.value
-                local_datetime = utc_datetime + dt.timedelta(seconds=time_offset)
+                local_datetime = utc_datetime + \
+                    dt.timedelta(seconds=time_offset)
                 newRow["utc_time"] = utc_datetime
                 newRow["local_time"] = local_datetime
             elif not data.name.startswith("unknown"):
-                newRow[data.name] = data.value
+                row_name = data.name
+                if(data.units):
+                    row_name = data.name + " [" + data.units + "]"
+                newRow[row_name] = data.value
         dataframe = dataframe.append(newRow, ignore_index=True)
     return dataframe
 
-IMPORT_FOLDER="import/"
-OUTPUT_FOLDER="export/"
-(_, _, filenames) = next(walk(IMPORT_FOLDER)) #retrieving file names from the files in the input folder
+
+IMPORT_FOLDER = "import/"
+OUTPUT_FOLDER = "export/"
+# retrieving file names from the files in the input folder
+(_, _, filenames) = next(walk(IMPORT_FOLDER))
 
 for filename in filenames:
-    print ("=============================================\n")
-    print("Parsing ",filename)
-    fitfile = fitparse.FitFile(IMPORT_FOLDER+filename) 
+    print("=============================================\n")
+    print("Parsing ", filename)
+    fitfile = fitparse.FitFile(IMPORT_FOLDER+filename)
     sw_version = getSwVersion(fitfile)
     file_type = getFileType(fitfile)
 
@@ -55,27 +66,30 @@ for filename in filenames:
         if(sys.argv[1] == "resample"):
             resample = True
 
-    if(file_type == "activity"): 
+    if(file_type == "activity"):
 
         time_offset = getTimeOffset(fitfile)
         dataframe = pandas.DataFrame()
-        dataframe = appendToDataframe("record", fitfile, time_offset, dataframe)
-        
+        dataframe = appendToDataframe(
+            "record", fitfile, time_offset, dataframe)
+
         if(resample):
-            dataframe.index = pandas.to_datetime(dataframe.local_time, format='%Y-%m-%D %H:%M:%S')
+            dataframe.index = pandas.to_datetime(
+                dataframe.local_time, format='%Y-%m-%D %H:%M:%S')
             dataframe = dataframe.resample("1S").asfreq()
 
         if not os.path.exists(OUTPUT_FOLDER):
             os.makedirs(OUTPUT_FOLDER)
 
         try:
-            dataframe.to_excel(OUTPUT_FOLDER+ os.path.splitext(filename)[0]+"_fitfilev"+str(sw_version)+".xlsx")
+            dataframe.to_excel(OUTPUT_FOLDER + os.path.splitext(filename)
+                               [0]+"_fitfilev"+str(sw_version)+".xlsx")
             print("SUCCESS: The converted file is located in the \"export\" folder.")
         except:
             print("ERROR: File could not be saved.")
-            print("If the file to be exported already exists, please make sure that the file is not open (e.g. in Excel) when running this script.") 
+            print("If the file to be exported already exists, please make sure that the file is not open (e.g. in Excel) when running this script.")
 
-    else: 
+    else:
         print("ERROR: This script converts files of type \"activity\" only.")
         print("Files of type \"", file_type, "\" can not be converted.")
 
